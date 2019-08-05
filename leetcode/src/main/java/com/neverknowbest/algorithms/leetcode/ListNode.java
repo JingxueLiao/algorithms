2ef347310aa1e4112a795bfd496d57b6b3ae350e
Jingxue Liao <jingxue.liao@gmail.com>
2019-08-06 00:56:18 +0800

    List Node

---
+++ leetcode/src/main/java/com/neverknowbest/algorithms/leetcode/ListNode.java
@@ -0,0 +1,10 @@
+package com.neverknowbest.algorithms.leetcode;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    
+    public ListNode(int x) {
+        val = x;
+    }
+}
