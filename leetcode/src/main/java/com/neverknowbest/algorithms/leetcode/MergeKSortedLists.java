02a2ce973e5bb89053e2f7c92b6aed43166b13ad
Jingxue Liao <jingxue.liao@gmail.com>
2019-10-16 23:49:17 +0800

    Solution of Merge k Sorted Lists

--- leetcode/src/main/java/com/neverknowbest/algorithms/leetcode/MergeKSortedLists.java
+++ leetcode/src/main/java/com/neverknowbest/algorithms/leetcode/MergeKSortedLists.java
@@ -1,4 +1,28 @@
 package com.neverknowbest.algorithms.leetcode;
 
+import java.util.*;
+
 public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, Comparator.comparingInt(o -> o.val));
+        for (int i = 0; i < lists.length; ++i) {
+            if (lists[i] != null) {
+                pq.offer(lists[i]);
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (!pq.isEmpty()) {
+            ListNode p = pq.poll();
+            cur.next = p;
+            cur = cur.next;
+            if (p.next != null) {
+                pq.offer(p.next);
+            }
+        }
+        return dummy.next;
+    }
 }
